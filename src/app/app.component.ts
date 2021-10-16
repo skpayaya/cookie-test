57239c45a8d627a442a30092f379b8640c1225d9
skpayaya <61133962+skpayaya@users.noreply.github.com>
2021-10-17 00:17:14 +0530

    1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cookie-service-example';
+  cookieValue: string;
+
+constructor(private cookieService: CookieService)
+  {
+    this.cookieService.deleteAll();
+    this.cookieService.set('Test', '0000000000000000000',{domain:'.github.com'});
+    this.cookieValue = this.cookieService.get('Test');
+
+    console.log(this.cookieValue);
+  }
 }
